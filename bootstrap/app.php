72d4fb1802c059f7dba8a3b99690029e8afd306d
Robinson L. Legaspi <robinson.legaspi@yahoo.com>
2016-10-05 17:06:43 +0800

    queue driver database

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,8 @@
 | the IoC container for the system binding all of the various parts.
 |
 */
+function pcntl_fork(){
+}
 
 $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
