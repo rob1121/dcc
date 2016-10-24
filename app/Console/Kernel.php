d06232d0082960ee54ff12389259131ea0ae63a9
Robinson L. Legaspi <robinson.legaspi@yahoo.com>
2016-10-24 16:22:42 +0800

    number format

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,9 +22,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('email:followup')->timezone('Asia/Manila')->dailyAt("08:00");
-        $schedule->command('email:followup')->timezone('Asia/Manila')->dailyAt("16:30");
-        $schedule->command('backup:run')->timezone('Asia/Manila')->dailyAt("15:50");
+        $schedule->command('email:followup')->timezone('Asia/Manila')->weekDays()->dailyAt("08:00");
     }
 
     /**
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
+el        $this->call(UserSeeder::class);
         $this->call(InternalSpecSeeder::class);
         $this->call(ExternalSpecSeeder::class);
         $this->call(IsoSeeder::class);
